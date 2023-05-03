459e1ed40cdb4695d0288a3c7efc45d10eb8c48b
David Alberto Mora <eukadaju@gmail.com>
2023-05-02 21:08:46 -0500

    up service

--- src/main/java/co/dalmope/usermicroservice/application/Constants.java
+++ src/main/java/co/dalmope/usermicroservice/application/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     public static final String PERSON_CREATED_MESSAGE = "Person created successfully";
     public static final String USER_CREATED_MESSAGE = "User created successfully";
     public static final String USER_DELETED_MESSAGE = "User deleted successfully";
-    public static final String RESPONSE_ERROR_MESSAGE_KEY = "error";
+    public static final String RESPONSE_ERROR_MESSAGE_KEY = "Error";
     public static final String NO_DATA_FOUND_MESSAGE = "No data found for the requested petition";
     public static final String PERSON_ALREADY_EXISTS_MESSAGE = "A person already exists with the DNI number provided";
     public static final String MAIL_ALREADY_EXISTS_MESSAGE = "A person with that mail already exists";
