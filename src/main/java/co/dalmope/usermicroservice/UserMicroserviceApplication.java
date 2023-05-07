ae05c5aa97910e1d5759ca1126a46bfdb91f4fa8
David Alberto Mora <eukadaju@gmail.com>
2023-05-07 10:40:50 -0500

    up service

--- src/main/java/co/dalmope/usermicroservice/UserMicroserviceApplication.java
+++ src/main/java/co/dalmope/usermicroservice/UserMicroserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserMicroserviceApplication {
-//up server
+//up service
 	public static void main(String[] args) {
 		SpringApplication.run(UserMicroserviceApplication.class, args);
 	}
