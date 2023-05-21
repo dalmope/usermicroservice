bc988fd350c3c1af37e86c5219884334480fbf25
David Alberto Mora <eukadaju@gmail.com>
2023-05-21 10:04:28 -0500

    add logger

--- src/main/java/co/dalmope/usermicroservice/infraestructure/entrypoints/http/controller/HealthController.java
+++ src/main/java/co/dalmope/usermicroservice/infraestructure/entrypoints/http/controller/HealthController.java
@@ -1,5 +1,6 @@
 package co.dalmope.usermicroservice.infraestructure.entrypoints.http.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,14 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/health")
+@Log4j
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class HealthController {
 
+    private final Logger logger = Logger.getLogger(HealthController.class.getName());
+
     @GetMapping()
     public ResponseEntity<String> health() {
+        logger.info("Health check");
         return new ResponseEntity<>("OK", HttpStatus.OK);
     }
     
