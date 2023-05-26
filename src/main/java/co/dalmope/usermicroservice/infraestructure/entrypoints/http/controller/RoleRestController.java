4ef44a58f78c324f74dae6625ad01d1836ee518d
David Alberto Mora <eukadaju@gmail.com>
2023-05-25 20:41:03 -0500

    crossOrigin

--- src/main/java/co/dalmope/usermicroservice/infraestructure/entrypoints/http/controller/RoleRestController.java
+++ src/main/java/co/dalmope/usermicroservice/infraestructure/entrypoints/http/controller/RoleRestController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import java.util.List;
 @RequestMapping("/role")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "jwt")
+@CrossOrigin("*")
 public class RoleRestController {
     private final IRoleResponseMapper roleResponseMapper;
     private final IRoleServicePort roleServicePort;
